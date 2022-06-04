0c0b10a5a440b5667be23668ad17a5fc36c97bd6
Robinson Roa <roa.robinson97@gmail.com>
2022-06-04 15:03:04 -0400

    Test table render and api service connection

--- src/__tests__/APIservice.test.js
+++ src/__tests__/APIservice.test.js
@@ -4,6 +4,6 @@ import DeckService from '../services/deckApi.service';
 describe('DECK API testing', () => {
   test('should pass', async () => {
     const response = await DeckService.newDeck();
-    console.log(response);
+    expect(response.status).toEqual(200);
   });
 });
