63269972b5a1aaa66d9673cc32d23112117bde22
Robinson Roa <roa.robinson97@gmail.com>
2022-06-04 10:33:55 -0400

    BrowserRouter added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React from 'react';
-
-import logo from './logo.svg';
+import {
+  BrowserRouter, Route, Link, Routes,
+} from 'react-router-dom';
+import Page1 from './pages/Page1';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<h1>Home Page</h1>} />
+        <Route exact path="page1" element={<Page1 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
